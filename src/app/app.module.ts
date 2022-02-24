892a54db94c3321fa425479d7ab1d7594a6aaf94
Jessy Pango <kvnjessy@gmail.com>
2022-02-24 03:39:12 +0100

    Ajout de composant: About et Contact + Installation de bootstrap@3.4.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    AboutComponent,
+    ContactsComponent
+  ], //  classes représentant le module. 
   imports: [
     BrowserModule,
     AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  ], // Pour importer d'autre modules
+  providers: [], // Pour déclarer les fabriques de services
+  bootstrap: [AppComponent] // Pour declarer le composant Racine du module
 })
 export class AppModule { }
